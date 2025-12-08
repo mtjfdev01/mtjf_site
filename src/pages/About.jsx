62b06ea343f5fd55d260f9b70f239ba633496a01
Mujeeb-ur-Rehman-dev <mujeeboff@gmail.com>
2025-12-08 17:06:13 +0500

    about page images update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import PageHeader from "../components/pageHeader/PageHeader";
-import image1 from '../assets/img/hero section for about/hero-about.webp'
+import image1 from '../assets/img/about/hero-image.webp'
 import viceChairmanImage from '../assets/img/about/yousaf_sb.webp'
 import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
 
