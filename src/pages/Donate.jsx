063752d239c791e1dfb40da8e170212752a00b89
mtjf development <dev@mtjfoundation.org>
2025-12-22 12:04:01 +0500

    updates

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -2,12 +2,18 @@ import PageHeader from '../components/pageHeader/PageHeader'
 import DonationProjectsMenu from '../components/donation/projects_menu'
 import Footer from '../components/footer/Footer'
 import DonateImage from '../assets/img/donate/donate.png'
+import DonateLgImage from '../assets/img/donate/donate_xs.png'
 
 
 const Donate = () => {
   return (
     <>
+    <div className='d-none md:d-block'>
+      <PageHeader title={'Main Donation Page'} image={DonateLgImage}/>
+    </div>
+    <div className='md:d-none'>
     <PageHeader title={'Main Donation Page'} image={DonateImage}/>
+    </div>
     <DonationProjectsMenu />
     <Footer />
     </>
