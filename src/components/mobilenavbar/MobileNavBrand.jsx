8d5841bbb8632331a7aa7d14ffd09055d65535c4
mtjf development <dev@mtjfoundation.org>
2026-06-17 12:32:25 +0500

    update of mobile navbar

--- src/components/mobilenavbar/MobileNavBrand.jsx
+++ src/components/mobilenavbar/MobileNavBrand.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import './mobilenavbar.css'
 
 const MobileNavBrand = () => (
-  <Link to="/home" className="mobile-nav-brand md:d-none" aria-label="Molana Tariq Jamil Foundation home">
+  <Link to="/home" className="mobile-nav-brand" aria-label="Molana Tariq Jamil Foundation home">
     <span className="mobile-nav-brand__accent" aria-hidden="true" />
     <span className="mobile-nav-brand__text">
       <span className="mobile-nav-brand__name">Molana Tariq Jamil</span>
