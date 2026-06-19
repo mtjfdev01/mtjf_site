bd6dca901821687273239cf4a7bd17766e6bcd09
mtjf development <dev@mtjfoundation.org>
2026-06-19 16:59:32 +0500

    updates

--- src/components/mobilenavbar/MobileNavBrand.jsx
+++ src/components/mobilenavbar/MobileNavBrand.jsx
@@ -3,10 +3,10 @@ import './mobilenavbar.css'
 
 const MobileNavBrand = () => (
   <Link to="/home" className="mobile-nav-brand" aria-label="Molana Tariq Jamil Foundation home">
-    <span className="mobile-nav-brand__accent" aria-hidden="true" />
+    {/* <span className="mobile-nav-brand__accent" aria-hidden="true" /> */}
     <span className="mobile-nav-brand__text">
-      <span className="mobile-nav-brand__name">Molana Tariq Jamil</span>
-      <span className="mobile-nav-brand__tagline">Foundation</span>
+      <span className="mobile-nav-brand__name">Molana Tariq Jamil Foundation</span>
+      {/* <span className="mobile-nav-brand__tagline">Foundation</span> */}
     </span>
   </Link>
 )
