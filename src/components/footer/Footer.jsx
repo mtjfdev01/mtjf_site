e633ce3dc24f58ac9421a50f208ad9e85eb57db3
Mujeeb-ur-Rehman-dev <mujeeboff@gmail.com>
2026-06-29 10:23:38 +0500

    updated footer text

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -121,7 +121,7 @@ const Footer = () => {
                     <span className="footer-link">0329787</span>
                   </li>
                   <div>
-                   <small>NPOs and associations must comply with Companies Regulations, 2024 (S.R.O. 210(I)/2024) Regulations 100(1)(xvii) by including “A Company set up under section 42 of the Companies Act, 2017”</small>
+                   <small>Molana Tariq Jamil Foundation is a Registered NGO under section 42 of Companies Act. 2017 (As Non-Profit Company)</small>
                   </div>
                   {/* <li>
                     <span className='footer-label'>MTJ Foundation Pakistan is a tax-exempt organization under Section 2(36)(c) of the FBR Act.</span>
